98f53ef15f35eed9166db7f14e6987386336b1c3
Danilo Miranda <danilomirandaa.15@outlook.com>
2023-02-27 09:28:20 -0300

    feat: learned about number

---
+++ day-01/MyPlayground.playground/Pages/NumberPlayground.xcplaygroundpage/Contents.swift
@@ -0,0 +1,36 @@
+import UIKit
+
+let score = 10
+
+let reallyBig = 10__00__00____00
+
+let lowerScore = score - 2
+let higherScore = score + 10
+let doableScore = score * 2
+let squaredScore = score * score
+let halvedScore = score / 2
+
+var counter = 10
+
+counter += 5
+
+print(counter)
+
+
+counter *= 2
+counter -= 10
+counter /= 2
+
+let number = 120
+print(number.isMultiple(of: 4))
+
+////////// Store Decimal Number  //////////////
+
+let a = 0.1
+let b = 0.2
+
+
+let res = Double(a) + Double(b)
+
+print(number)
+
---
+++ day-01/MyPlayground.playground/Pages/StringPlayground.xcplaygroundpage/Contents.swift
@@ -0,0 +1,26 @@
+import UIKit
+
+
+let actor = "Jonny Depp"
+
+
+let filename = "paris.jpg"
+
+let result = "you win!"
+
+
+let movie = """
+A day in
+the life of an
+Apple engineer
+"""
+
+print(result.uppercased())
+
+
+
+
+print(movie.hasPrefix("A day")) // Expected return true, because is this string in this variable
+
+
+print(filename.hasSuffix(".jpg"))
