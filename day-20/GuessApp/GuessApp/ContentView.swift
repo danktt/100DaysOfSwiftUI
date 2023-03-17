c2f1c79cd481354324a4992dbe2c0757ddb06465
Danilo Miranda <danilomirandaa.15@outlook.com>
2023-03-17 19:44:35 -0300

    feat: add new day 20

---
+++ day-20/GuessApp/GuessApp/ContentView.swift
@@ -0,0 +1,32 @@
+//
+//  ContentView.swift
+//  GuessApp
+//
+//  Created by Dan on 17/03/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        
+            VStack {
+                Text("Hello, world!").padding()
+               
+                Text("This is another text view")
+                Text("This is another text view")
+                Text("This is another text view")
+                Text("This is another text view")
+                Text("This is another text view")
+            }
+        }
+        
+        
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ day-20/GuessApp/GuessApp/GuessAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessAppApp.swift
+//  GuessApp
+//
+//  Created by Dan on 17/03/23.
+//
+
+import SwiftUI
+
+@main
+struct GuessAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
