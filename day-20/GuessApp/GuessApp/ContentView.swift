ad99f934b151f664caf53d64a6c28e2e428d60b6
Danilo Miranda <danilomirandaa.15@outlook.com>
2023-03-18 22:13:22 -0400

    feat: add new folder, day 21

--- day-20/GuessApp/GuessApp/ContentView.swift
+++ day-20/GuessApp/GuessApp/ContentView.swift
@@ -8,20 +8,21 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        
-            VStack {
-                Text("Hello, world!").padding()
-               
-                Text("This is another text view")
-                Text("This is another text view")
-                Text("This is another text view")
-                Text("This is another text view")
-                Text("This is another text view")
+    var body: some View {NavigationView{
+        Section{
+            ScrollView{
+                VStack(spacing: 40) {
+                    Text("Hello, world!").padding()
+                    
+                    Text("This is another text view")
+                    Text("This is another text view")
+                    Text("This is another text view")
+                    Text("This is another text view")
+                    Text("This is another text view")
+                    
+                }
             }
-        }
-        
-        
+        }}
     }
 }
 
---
+++ day-21/Guess/Guess/GuessApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessApp.swift
+//  Guess
+//
+//  Created by Dan on 18/03/23.
+//
+
+import SwiftUI
+
+@main
+struct GuessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
