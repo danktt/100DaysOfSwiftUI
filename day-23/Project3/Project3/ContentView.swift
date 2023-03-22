f518a4b1a7647aac617aa78fea6fd54e6ed23826
Danilo Miranda <danilomirandaa.15@outlook.com>
2023-03-21 23:25:44 -0400

    feat: add new folder day-24

---
+++ day-23/Project3/Project3/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  Project3
+//
+//  Created by Dan on 20/03/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Button("Hello!!"){
+            
+        }
+        .padding()
+        .background(.red)
+        .padding()
+        .background(.blue)
+    }
+}
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ day-23/Project3/Project3/Project3App.swift
@@ -0,0 +1,17 @@
+//
+//  Project3App.swift
+//  Project3
+//
+//  Created by Dan on 20/03/23.
+//
+
+import SwiftUI
+
+@main
+struct Project3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
