a18b22adc77b50ad610de8f82d6560993ddb80a8
Danilo Miranda <danilomirandaa.15@outlook.com>
2023-03-21 23:43:01 -0400

    feat: add new folder day 22, add new app GuessFlag        add new styles on app, styles of the button"
    This reverts commit a6f4065d5a06cd09f7f6b717fe843fa6952c1c1b.

---
+++ day-22/GuessApp22/GuessApp22/GuessApp22App.swift
@@ -0,0 +1,17 @@
+//
+//  GuessApp22App.swift
+//  GuessApp22
+//
+//  Created by Dan on 19/03/23.
+//
+
+import SwiftUI
+
+@main
+struct GuessApp22App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
