ea470fd2a6d11a248131d2aaed71e867fe948d91
Danilo Miranda <danilomirandaa.15@outlook.com>
2023-03-29 23:28:57 -0400

    feat: add new day 32

---
+++ day-32/Animations/Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Dan on 29/03/23.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
