5c578cbc6f3de70646be9f3c0a6d82a33741ba6e
Danilo Miranda <danilomirandaa.15@outlook.com>
2023-03-30 23:24:53 -0400

    feat: add new day 33, animations

---
+++ day-33/animations2/animations2/ContentView.swift
@@ -0,0 +1,40 @@
+//
+//  ContentView.swift
+//  animations2
+//
+//  Created by Dan on 30/03/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    let letters = Array("Hello SwiftUI")
+        @State private var enabled = false
+        @State private var dragAmount = CGSize.zero
+    var body: some View {
+        HStack(spacing: 0) {
+                    ForEach(0..<letters.count, id: \.self) { num in
+                        Text(String(letters[num]))
+                            .padding(5)
+                            .font(.title)
+                            .background(enabled ? .blue : .red)
+                            .offset(dragAmount)
+                            .animation(.default.delay(Double(num) / 20), value: dragAmount)
+                    }
+                }
+                .gesture(
+                    DragGesture()
+                        .onChanged { dragAmount = $0.translation }
+                        .onEnded { _ in
+                            dragAmount = .zero
+                            enabled.toggle()
+                        }
+                )
+            }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ day-33/animations2/animations2/animations2App.swift
@@ -0,0 +1,17 @@
+//
+//  animations2App.swift
+//  animations2
+//
+//  Created by Dan on 30/03/23.
+//
+
+import SwiftUI
+
+@main
+struct animations2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
