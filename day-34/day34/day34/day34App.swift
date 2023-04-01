169f97ddee5d10b7b05508296e4f19f5f3f4a2fa
Danilo Miranda <danilomirandaa.15@outlook.com>
2023-03-31 21:37:25 -0400

    feat: add new day 34

---
+++ day-34/day34/day34/day34App.swift
@@ -0,0 +1,17 @@
+//
+//  day34App.swift
+//  day34
+//
+//  Created by Dan on 31/03/23.
+//
+
+import SwiftUI
+
+@main
+struct day34App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
