6bf5a06a2445b8ce051c4d9bf70e8527604d3baf
Danilo Miranda <danilomirandaa.15@outlook.com>
2023-04-30 18:18:34 -0400

    Revert "feat: add some folders day's" Feat: revert commit
    This reverts commit a7fe9432492441b497853aa37574673ca27ce3cc.

--- day-43/Drawing/Drawing/ContentView.swift
+++ day-43/Drawing/Drawing/ContentView.swift
@@ -9,59 +9,39 @@ import SwiftUI
 
 
 
-struct Flower: Shape {
-    // How much to move this petal away from the center
-    var petalOffset: Double = -20
-
-    // How wide to make each petal
-    var petalWidth: Double = 100
-
+struct Triangle: Shape {
     func path(in rect: CGRect) -> Path {
-        // The path that will hold all petals
         var path = Path()
 
-        // Count from 0 up to pi * 2, moving up pi / 8 each time
-        for number in stride(from: 0, to: Double.pi * 2, by: Double.pi / 8) {
-            // rotate the petal by the current value of our loop
-            let rotation = CGAffineTransform(rotationAngle: number)
+        path.move(to: CGPoint(x: rect.midX, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.midX, y: rect.minY))
 
-            // move the petal to be at the center of our view
-            let position = rotation.concatenating(CGAffineTransform(translationX: rect.width / 2, y: rect.height / 2))
+        return path
+    }
+}
 
-            // create a path for this petal using our properties plus a fixed Y and height
-            let originalPetal = Path(ellipseIn: CGRect(x: petalOffset, y: 0, width: petalWidth, height: rect.width / 2))
+struct Arc: Shape {
+    var startAngle: Angle
+    var endAngle: Angle
+    var clockwise: Bool
 
-            // apply our rotation/position transformation to the petal
-            let rotatedPetal = originalPetal.applying(position)
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        path.addArc(center: CGPoint(x: rect.midX, y: rect.midY), radius: rect.width / 2, startAngle: startAngle, endAngle: endAngle, clockwise: clockwise)
 
-            // add it to our main path
-            path.addPath(rotatedPetal)
-        }
-
-        // now send the main path back
         return path
     }
 }
 
 struct ContentView: View {
-    @State private var petalOffset = -20.0
-    @State private var petalWidth = 100.0
-
     var body: some View {
-        VStack {
-            Flower(petalOffset: petalOffset, petalWidth: petalWidth)
-                .fill(.orange, style: FillStyle(eoFill: true))
-
-            Text("Offset")
-            Slider(value: $petalOffset, in: -40...40)
-                .padding([.horizontal, .bottom])
-
-            Text("Width")
-            Slider(value: $petalWidth, in: 0...100)
-                .padding(.horizontal)
-        }
+        Arc(startAngle: .degrees(0), endAngle: .degrees(110), clockwise: true)
+            .stroke(.blue, lineWidth: 10)
+            .frame(width: 300, height: 300)
+            
     }
-}
     
     struct ContentView_Previews: PreviewProvider {
         static var previews: some View {
@@ -71,4 +51,4 @@ struct ContentView: View {
     
     
     
-
+}
